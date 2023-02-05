8332a6e7cbf619426dd76ee14f8d05d77faf1524
LaylaSawan <123342645+LaylaSawan@users.noreply.github.com>
2023-02-05 14:43:40 -0500

    changes to sensor

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 async function getListOfSensorValues() { 
 
-    fetch('http://localhost:3000/sensor')
-        .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                document.getElementById("sensor").innerHTML = data["value"];
+    fetch('http://localhost:3000/sensor')// its is making a GET request
+        .then(res => res.json())//when you get the responce from the api, run this; it converts responce to json
+            .then(data => {//when you finish converting the json, run the code
+                console.log(data)// its printing the data
+                document.getElementById("sensor").innerHTML = data["value"];// acsess the tag with id sensor and; putting the data into the value
             })
 } 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require('express');// expess is used to create a mini api
 
-const app = express(); 
+const api = express();// this line cerates the api
 
 var sql = require('mysql'); // grabbing the mysql library (functions and variables) and saving it into the variable sql 
-var cors = require('cors');
+var cors = require('cors');// its helps you filter and manipulate the data
 
-app.use(cors())
+api.use(cors())
 //is preparing the sql connection
 var con = sql.createConnection({
     host: "composting.chsfz35kckgj.us-east-1.rds.amazonaws.com",
@@ -17,18 +17,18 @@ var con = sql.createConnection({
 
 con.connect();
 
-app.get('/sensor', (request, response) =>{
-    con.query("SELECT * " + 
-              "FROM Sensor " +
-              "ORDER BY recorded DESC " +
-              "LIMIT 1", function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        response.send(result[0]);  
+api.get('/sensor', (request, response) =>{  //it is creating an event; when im making a GET request on localhost:3000/sensor
+    con.query("SELECT * " +// * means select all the columns 
+              "FROM Sensor " + // this is from the table sensor
+              "ORDER BY recorded DESC " + // it is ordering by the datetime DESC means that we should get the latest data
+              "LIMIT 1", function (err, result) { // LIMIT 1 means that their should only be one reading
+        if (err) throw err; // if their is an error, abort the code 
+        console.log(result); // printing the result on the server side
+        response.send(result[0]);// it is sending the result  
       });
 })
 
-app.listen(3000, function () { 
+api.listen(3000, function () { // it is starting the api on port 3000
     console.log('The mini API is listening on port 3000'); 
 }); 
 
