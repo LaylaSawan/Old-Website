2061110b699fd0d5e7fc06b282db8c4b7c84f955
LaylaSawan <123342645+LaylaSawan@users.noreply.github.com>
2023-04-02 15:52:37 -0400

    changes to website

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 setInterval(getListOfSensorValues, 5000);
 async function getListOfSensorValues() { 
 
-    fetch('http://localhost:3000/sensor')// its is making a GET request
+    fetch('https://cvpcmc3kka.execute-api.us-east-1.amazonaws.com/prod')// its is making a GET request
         .then(res => res.json())//when you get the responce from the api, run this; it converts responce to json
             .then(data => {//when you finish converting the json, run the code
                 console.log(data["value"])// its printing the data
                 
-                let dataBaseValues = data["value"];
+                let dataBaseValues = data["value"]; // let = create a variable
                 const myValue = dataBaseValues.split(",");
                 document.getElementById("bottomMoistureSensor").innerHTML = (Math.round(((1700 - parseInt(myValue[0])) / 1700) * 10000) / 100) + "%";// acsess the tag with id sensor and; putting the data into the value
                 document.getElementById("topMoistureSensor").innerHTML = Math.round(((4100 - parseInt(myValue[1])) / 4100) * 10000) / 100+ "%";
                 document.getElementById("tempSensorValue").innerHTML = myValue[2] += "&#8451";
                 document.getElementById("photoSensorValue").innerHTML = (Math.round((( parseInt(myValue[3])) / 4000) * 10000) / 100) + "%";
+
             })
 } 
+ 
